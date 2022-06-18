cacfb9271d911f9dbcc5fa043831865b2a983bc4
Gnlow <gnlow@protonmail.com>
2022-06-18 13:11:18 +0900

    성공!
    http://wiki.shtelo.org/index.php/%EB%AA%A8%EB%93%88:Dksemfhdj

--- src/mod.ts
+++ src/mod.ts
@@ -11,20 +11,17 @@ const 종성 = " r R rt s sw sg e f fr fa fq ft fx fv fg a q qt t T d w c z x v 
 
 function charCodeAt(char: string) {
     const [a, b, c] = string.byte(char, 1, 3)
-    print(a, b, c)
-    return ((a - 224) << 12) + ((b - 128) << 6) + c - 128 - 44032
+    return ((a - 224) * 4096) + ((b - 128) * 64) + c - 128 - 44032
 }
 
-function dksemfh(text: string) {
-    let result = ""
-    for (const [char] of string.gmatch(text, "...")) {
+function dksemfhdj(text: string) {
+    const [result] = string.gsub(text, "[^%c%p%s%w][^%c%p%s%w][^%c%p%s%w]", (char) => {
         let code = charCodeAt(char)
         let 초, 중, 종
         code -= (초 = Math.floor(code / 588)) * 588
         code -= (중 = Math.floor(code / 28)) * 28
         종 = code
-        result += 초성[초] + 중성[중] + 종성[종]
-    }
+        return 초성[초] + 중성[중] + 종성[종]
+    })
     return result
 }
-print(dksemfh("각"))
